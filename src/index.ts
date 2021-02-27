ff3e57b42ad0f5fdd38d9ae99cde5335492ed8ab
Arsen105 <47545695+Arsen105@users.noreply.github.com>
2021-02-27 05:33:40 +0300

    jest test all files

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,15 @@ import './assets/main.scss'
 import './js/View/subView'
 
 let s = new SliderPlag('.slider_rqik').data({ rotate: 'vertical', show: true, showInterval: true })
-// setTimeout(() => {
-//   s.data({showInterval: false})
-// } , 2000)
-// setTimeout(() => {
-//   s.data({showInterval: true})
-// } , 4000)
-// setTimeout(() => {
-//   s.data({showInterval: false , show: false})
-// } , 6000)
+setTimeout(() => {
+  s.data({showInterval: false})
+} , 2000)
+setTimeout(() => {
+  s.data({showInterval: true})
+} , 4000)
+setTimeout(() => {
+  s.data({ rotate: 'horizontal'})
+} , 6000)
 
 
  new SliderPlag('.slider2').data({rotate: 'horizontal'})
